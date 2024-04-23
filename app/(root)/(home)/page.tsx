ec2107aab49afcbbfb8dd912c492be8c3bb9a687
coderohan316 <rohanx316@gmail.com>
2024-04-23 21:02:59 +0530

    GMT to IST

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,8 +5,10 @@ const Home = () => {
   const now = new Date();
 
   const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeZone: 'Asia/Kolkata' })).format(now);
 
-  const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full' })).format(now);
+  //const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full' })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
