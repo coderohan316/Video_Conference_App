44fd4bcb4318dc6cffc31f3c65fc94bba99a47b3
coderohan316 <rohanx316@gmail.com>
2024-04-29 00:12:08 +0530

    ..

--- components/ui/MobileNav.tsx
+++ components/ui/MobileNav.tsx
@@ -38,7 +38,7 @@ const MobileNav = () => {
                      className='max-sm:size-10'
                />
 
-                 <p className='text-[26px] font-extrabold text-white'>ConCon</p>
+                 <p className='text-[26px] font-extrabold text-white'>ConCat</p>
                  </Link>
 
                  <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
