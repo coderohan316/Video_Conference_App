1c8eea8dcdb6ca4bcabb60ba8c113cb186ee8e2a
coderohan316 <rohanx316@gmail.com>
2024-05-05 18:29:04 +0530

    time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -17,7 +17,8 @@ const Home = () => {
           {/* <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Upcoming Meeting at: 12:30</h2> */}
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">
-              {time}
+              {time} (GMT)
+              
             </h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
